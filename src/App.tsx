3ae92c109831dce88b4d0ad951aa70e73fdde0b3
Steven Wen <73114262+loopfree@users.noreply.github.com>
2024-03-07 18:55:32 +0700

    fix: fix missing code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 
+import "@fontsource/source-sans-pro/600.css";
 import "@fontsource/tektur/500.css";
 
 function App() {
