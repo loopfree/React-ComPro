3eab16148eee335564eae7c9526163eee963f054
Steven Wen <73114262+loopfree@users.noreply.github.com>
2024-03-07 18:39:53 +0700

    refactore: remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
-import "./App.css";
-import "@fontsource/source-sans-pro/200.css";
-import "@fontsource/source-sans-pro/300.css";
-import "@fontsource/source-sans-pro/400.css";
-import "@fontsource/source-sans-pro/600.css";
-import "@fontsource/source-sans-pro/700.css";
-import "@fontsource/source-sans-pro/900.css";
 
-import "@fontsource/tektur/400.css";
 import "@fontsource/tektur/500.css";
 
 function App() {
