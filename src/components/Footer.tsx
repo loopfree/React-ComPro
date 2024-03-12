bd2f1dcbbb08bb23d599f2d50fbb9b3599b52381
Steven Wen <73114262+loopfree@users.noreply.github.com>
2024-03-12 17:26:50 +0700

    feat: update footer to adjust with other page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,20 @@
 import { Box, Text } from "@chakra-ui/react";
 
-const Footer = () => {
+type FooterProps = {
+  position: "static" | "relative" | "absolute" | "fixed" | "sticky";
+};
+
+const Footer: React.FC<FooterProps> = ({ position }) => {
   return (
     <Box
       py={{ base: 5, md: 6, lg: 6 }}
       px={{ base: 10, md: 24, lg: 24 }}
       bg="purple.400"
       h={{ base: "10vh", md: "6vh", lg: "10vh" }}
+      position={position}
+      bottom="0"
+      left="0"
+      right="0"
     >
       <Text
         color="white"
